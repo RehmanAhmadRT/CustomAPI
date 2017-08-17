7c8fd5e568f7cf023f2f60720f25977c12f6e2f1
Rehman Ahmad <rehman.ahmad@RT-LT-268.rolustech.net>
2017-08-17 17:46:01 +0500

    Custom REST API

---
+++ clients/base/views/user_opportunities/user_opportunities.php
@@ -0,0 +1,15 @@
+<?php
+$viewdefs['base']['view']['user_opportunities'] = array(
+    'dashlets' => array(
+        array(
+            'label' => 'user opportunities!',
+            'description' => 'user_opportunities is a basic dashlet for the SugarCRM.',
+            'config' => array(
+            ),
+            'preview' => array(
+            ),
+            'filter' => array(
+            )
+        ),
+    ),
+);
